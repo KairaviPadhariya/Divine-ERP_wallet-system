cead77a745e325a184561f09a9826f5de7fa9f98
KairaviPadhariya <kairavipadhariya@gmail.com>
2026-01-08 12:11:18 +0530

    Add new components

---
+++ src/components/AdminDashboard.jsx
@@ -0,0 +1,10 @@
+const AdminDashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Admin Dashboard</h1>
+      <p>Welcome Admin 👑</p>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ src/components/UserDashboard.jsx
@@ -0,0 +1,10 @@
+const UserDashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>User Dashboard</h1>
+      <p>Welcome User 👋</p>
+    </div>
+  );
+};
+
+export default UserDashboard;
